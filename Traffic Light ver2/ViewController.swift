4878e235d988017212ee1ea8ba0b3ab56f93b460
popov <popich2005@mail.ru>
2024-01-31 12:42:35 +0300

    Initial Commit

---
+++ Traffic Light ver2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic Light ver2
+//
+//  Created by Алексей Попов on 31.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
